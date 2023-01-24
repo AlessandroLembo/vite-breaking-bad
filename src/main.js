5ce3f7e8b091ca925bd60f03b558de5415becdbb
AlessandroLembo <alessandrolembo2@gmail.com>
2023-01-24 15:47:40 +0100

    imported Font awesome

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import './style.css'
+
+import {library} from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faSpinner);
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.component('font-awesome-icon', FontAwesomeIcon);
+app.mount('#app');
