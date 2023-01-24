f7a9c6f0ab2084c9e7edc7fbd3b6ffc9cdee26dc
AlessandroLembo <alessandrolembo2@gmail.com>
2023-01-24 16:31:28 +0100

    header and main blocks

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,13 +1,23 @@
 <script>
 export default {
+    name: 'AppHeader',
 
 }
 </script>
 
 <template>
+    <header>
+        <div class="header-container">
 
+        </div>
+    </header>
 </template>
 
 <style scoped lang="scss">
+@use '../assets/scss/partials/variables';
 
+.header-container {
+    height: 150px;
+    border: 1px dashed black;
+}
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,13 +1,24 @@
 <script>
 export default {
+    name: 'AppMain',
 
 }
 </script>
 
 <template>
+    <main>
+        <div class="main-container">
+
+        </div>
+    </main>
 
 </template>
 
 <style scoped lang="scss">
+@use '../assets/scss/partials/variables';
 
+.main-container {
+    border: 1px solid black;
+    height: calc(100vh - 150px);
+}
 </style>
